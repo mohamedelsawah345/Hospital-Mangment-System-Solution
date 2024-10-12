8ab3669e647f4a313e0e82c0a9b2c7b1662a78c3
Mohamed El-sawah <melsawsh345@gmail.com>
2024-10-12 05:26:47 +0300

    change in commit 2 which was is " test 2 in stage 1"This is test 1 for changes this  is  contain on 2 new classes Test  save changes and Test save chnges deal with githup

---
+++ Hospital Mangment System_PL/TestGitHupChanges/TestSaveChangesAndDealWithGitHup.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital_Mangment_System_PL.TestGitHupChanges
+{
+    public class TestSaveChangesAndDealWithGitHup
+    {
+
+        //this is the frist changes in Hospital Mangment System which i write this is comment okay?
+
+
+
+
+    }
+}
