07b8b113b88d565b6f55876bf5898abe7422e516
ahmed abnaser <ahmeducfjf@gmail.com>
2024-10-12 22:12:41 +0300

    AddmissionPropertyies

---
+++ Hospital Mangment System DAL/Entites/addmission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Mangment_System_DAL.Entites
+{
+    public class addmission
+    {
+        public int Addmission_Id { get; set; }
+        public int room_number { get; set; }
+        public DateOnly Addmission_Date { get; set; }
+        public DateOnly Date_discharge { get; set; }
+        public int Patient_Id { get; set; }
+
+        public patient patient { get; set; } = null;//ameend test
+    }
+}
