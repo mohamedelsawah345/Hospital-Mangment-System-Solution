657f03375fdb037530129472c3e19ecc4e7f80cf
ahmed abnaser <ahmeducfjf@gmail.com>
2024-10-13 15:36:16 +0300

    CommentTest

--- Hospital Mangment System DAL/Entites/addmission.cs
+++ Hospital Mangment System DAL/Entites/addmission.cs
@@ -12,7 +12,10 @@ namespace Hospital_Mangment_System_DAL.Entites
         public DateOnly Addmission_Date { get; set; }
         public DateOnly Date_discharge { get; set; }
         public int Patient_Id { get; set; }
-
+        //test 1 2 3
+        /// <summary>
+        /// dasdsdad
+        /// </summary>
         public Patient patient { get; set; } = null;//ameend test
     }
 }
