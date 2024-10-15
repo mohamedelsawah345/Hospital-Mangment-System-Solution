2e54ddb94e64162b710faeca4acdc83a005b41a2
Mohammed Albasuony <mido.beso.hosny@gmail.com>
2024-10-15 23:00:05 +0300

    Last edit on Get Department by id

--- Hospital Mangment System BLL/View model/DepartmentVM/GetDepartmentByIdVM.cs
+++ Hospital Mangment System BLL/View model/DepartmentVM/GetDepartmentByIdVM.cs
@@ -11,5 +11,7 @@ namespace Hospital_Mangment_System_BLL.View_model.DepartmentVM
         public int? Dnum { get; set; }
         public string? Dname { get; set; }
         public string? Location { get; set; }
+        public List<string>? DoctorName { get; set; }
+        public List<string>? NurseName { get; set; }
     }
 }
