5c0426a6cd43dfa3431174a0a7193a0a2144665d
Ahmedabnaser <ahmeducfjf@gmail.com>
2024-11-04 14:42:21 +0200

    test

--- Hospital Mangment System_PL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hospital_Mangment_System_PL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Hospital Mangment System_PL/TestGitHupChanges/TestSaveChanges.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hospital_Mangment_System_PL.TestGitHupChanges
-{
-    public class TestSaveChanges
-    {
-
-
-        //chanage in comit 2 which was is " //test2 in stage 1"
-    }
-}
